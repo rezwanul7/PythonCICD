1d38e6a561a2c26c91833be7fa62d86033dad5f4
Rezwanul Islam <rezwanul7@gmail.com>
2023-12-25 11:37:08 +0600

    refactor deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-# Go to your app directory
-cd ~/PythonCICD
-
-# Pull the latest code
-git pull origin develop
-git checkout develop
-git status
-
 # Install dependencies
 poetry install
 
