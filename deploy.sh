fdb951fcd7d5159396accf4234269d55145bd1dc
Rezwanul Islam <rezwanul7@gmail.com>
2024-01-02 23:58:01 +0600

    enhance deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Ensure that the script exits immediately if any command it runs exits with a non-zero status.
+set -e
+
 # Install dependencies
 export PATH=$HOME/.local/bin:$PATH
 poetry install
@@ -6,3 +10,5 @@ poetry install
 # Restart the FastAPI app
 echo "restarting the app"
 systemctl restart python_cicd_app.service
+
+echo "Deployment completed successfully."
