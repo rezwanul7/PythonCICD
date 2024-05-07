a7a569db806d2552af3444133be32395f146edea
Rezwanul Islam <rezwanul7@gmail.com>
2024-05-07 23:20:09 +0600

    refactor code

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker compose -f common.yaml -f compose.stag.yaml config
-docker compose -f common.yaml -f compose.stag.yaml pull
-docker compose -f common.yaml -f compose.stag.yaml up -d
-echo "Deployment completed successfully."
